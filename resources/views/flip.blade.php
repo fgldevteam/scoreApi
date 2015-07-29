d41b0ae5f4ff380e86785036be9fe9349c54ce23
Brent Garner <bgarner@gmail.com>
2015-07-29 16:28:26 -0600

    change jquery from CDN to local

--- resources/views/flip.blade.php
+++ resources/views/flip.blade.php
@@ -26,7 +26,7 @@
 		</div>
 
 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+        <script src="js/jquery.min.js"></script>
         <script src="js/jquery.boxroll-slider.js"></script>
 		<script src="js/utils.js"></script>
 		
